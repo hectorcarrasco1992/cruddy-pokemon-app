16fb43258c4cc2729698adfc2369d05afe68b187
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-12-16 01:20:37 -0500

    fixed indentations

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 document.querySelector('.s-button').addEventListener('click',()=>{
     const searchInput = document.querySelector('input').value
-    // const newSrc = `./images/Pikachu`
-    // let resultImage = document.querySelector('.result')
-
-    // resultImage.src= `${newSrc}`
+    
 
     document.querySelector('#result-pic').src = `images/${searchInput}`
 })
